1d5b76825f4bd56ae2de92e09254d65e18f3cde7
badewa kayode <badewakayode1@gmail.com>
2018-04-04 07:52:36 +0100

    update hotel schema

--- models/hotels.js
+++ models/hotels.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const hotelSchema = new Schema({
-	hotel_name: {type: String, required: false},
-	location: {type: String, required: false},
-	city: {type: String, required: false},
-	state: {type: String, required: false},
-	present_price: {type: String, required: false},
+	hotel_name: {type: String, required: true},
+	location: {type: String, required: true},
+	city: {type: String, required: true},
+	state: {type: String, required: true},
+	present_price: {type: String, required: false,default:"₦--.--"},
 	features: {type: [String]},
 	updated:{type:Boolean,default:false},
-	hotelsNg_link: {type: String, required: false},
+	hotelsNg_link: {type: String, required: true},
 	description:String,
 	room_types: [{name:String,facilities:[String],price:String}]
 });
