2717d6070b7ff93c65240e1c0e668485e039be9c
badewa kayode <badewakayode1@gmail.com>
2018-04-16 16:22:12 +0100

    Api v1 route added

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,18 @@ Router.get('/city/:city/', controller.findByCity);
 Router.get('/name/:name/:page([0-9]*)/', controller.findByName);
 Router.get('/name/:name/', controller.findByName);
 
+//api v1
+Router.get('/api/v1/', controller.index);
+Router.get('/api/v1/:page([0-9])', controller.index);
+
+Router.get('/api/v1/state/:state/:page([0-9]*)', controller.findByState);
+Router.get('/api/v1/state/:state/', controller.findByState);
+
+Router.get('/api/v1/city/:city/:page([0-9]*)', controller.findByCity);
+Router.get('/api/v1/city/:city/', controller.findByCity);
+
+Router.get('/api/v1/name/:name/:page([0-9]*)/', controller.findByName);
+Router.get('/api/v1/name/:name/', controller.findByName);
 //api v2
 Router.get("/api/v2/",controller.apiV2Index)
 Router.get("/api/v2/:id",controller.apiV2Single)
